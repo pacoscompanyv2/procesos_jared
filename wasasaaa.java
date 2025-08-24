b4dac0d23107c2094251d7127a7e7ed1660b40be
Jared Fernandez <fernandezgonzalezjared@gmail.com>
2025-08-24 11:47:53 -0600

    first commit

---
+++ wasasaaa.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class wasasaaa {
+    public static void main(String[] args) {
+        Scanner ws = new Scanner(System.in);
+        int n1, n2,r;
+        System.out.println("introduce num 1");
+        n1=ws.nextInt();
+System.out.println("num 2");
+n2=ws.nextInt();
+r=n1+n2;
+System.out.println("el resultado es "+r);
+    }
+}
